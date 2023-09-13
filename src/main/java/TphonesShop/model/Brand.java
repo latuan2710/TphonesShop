4e5760861320fe1cdfda73aa6fdf7e1eea542d2d
latuan2710 <109055879+latuan2710@users.noreply.github.com>
2023-09-13 20:54:23 +0700

    update

--- src/main/java/TphonesShop/model/Brand.java
+++ src/main/java/TphonesShop/model/Brand.java
@@ -1,10 +1,15 @@
 package TphonesShop.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -19,14 +24,18 @@ public class Brand {
 	private String name;
 	@Column(name = "image")
 	private String image;
+	@OneToMany(mappedBy = "brand", cascade = CascadeType.ALL)
+	private List<Product> products;
 
 	public Brand() {
+		this.products = new ArrayList<>();
 	}
 
 	public Brand(String name, String image) {
 		super();
 		this.name = name;
 		this.image = image;
+		this.products = new ArrayList<>();
 	}
 
 	public long getId() {
@@ -51,6 +60,14 @@ public class Brand {
 
 	public void setImage(String image) {
 		this.image = image;
+	}
+
+	public List<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(List<Product> products) {
+		this.products = products;
 	}
 
 	@Override
--- src/main/java/TphonesShop/model/Product.java
+++ src/main/java/TphonesShop/model/Product.java
@@ -2,11 +2,15 @@ package TphonesShop.model;
 
 import java.text.DecimalFormat;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -17,7 +21,10 @@ public class Product {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	private String name;
-	private String brand;
+	@ManyToOne
+	@JoinColumn(name = "brand_id")
+	@JsonIgnore
+	private Brand brand;
 	private int quantity;
 	private double price;
 	private double final_price;
@@ -47,11 +54,11 @@ public class Product {
 		this.name = name;
 	}
 
-	public String getBrand() {
+	public Brand getBrand() {
 		return brand;
 	}
 
-	public void setBrand(String brand) {
+	public void setBrand(Brand brand) {
 		this.brand = brand;
 	}
 
